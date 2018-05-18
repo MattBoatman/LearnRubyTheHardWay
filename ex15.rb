f2b292198157abbc566b382dc26d80a4b524331b
Matt Boatman <matthewd.boatman@gmail.com>
2018-05-17 22:38:32 -0400

    do some more

--- ex15.rb
+++ ex15.rb
@@ -4,9 +4,11 @@ txt = open(filename)
 
 puts "Here's your file #{filename}:"
 print txt.read
+close(txt)
 print "Type the filename again: "
 file_again = $stdin.gets.chomp
 
 txt_again = open(file_again)
 
 print txt_again.read
+close(txt_again)
---
+++ ex16.rb
@@ -0,0 +1,27 @@
+filename = ARGV.first
+
+puts "We're going to erase #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file..."
+target = open(filename, 'w')
+
+puts "Truncating the file. Goodbye!"
+target.truncate(0)
+puts "Now Im going to ask you for three lines."
+
+print "Line 1: "
+line1 = $stdin.gets.chomp
+print "Line 2: "
+line2 = $stdin.gets.chomp
+print "Line 3: "
+line3 = $stdin.gets.chomp
+
+puts "Im going to write these to the file."
+target.write("#{line1}\n#{line2}\n#{line3}")
+
+puts "And finally, we close it."
+target.close
